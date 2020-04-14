246823586dd32f4962ab4709181d3279341a9e3e
Noah Ross <noah.baine@gmail.com>
2020-04-13 20:49:44 -0700

    commiting basic flask app per project guide

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 import os
-from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from models import setup_db
 from flask_cors import CORS
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
 
-  return app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-APP = create_app()
+    @app.route('/')
+    def get_greeting():
+        # excited = os.environ['EXCITED']
+        excited = 'true'
+        greeting = "Hello" 
+        if excited == 'true': greeting = greeting + "!!!!!"
+        return greeting
 
-@app.route("/")
-def test_home():
-  return "<h1>Did it work?</h1>"
+    @app.route('/coolkids')
+    def be_cool():
+        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run()
