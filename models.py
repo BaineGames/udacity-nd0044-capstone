fa9051bf97eae07861de91a8db9365f22eae1afd
Noah Ross <noah.baine@gmail.com>
2020-04-13 21:03:50 -0700

    adapted models to fit casting spec

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import Column, String, create_engine, Integer
+from sqlalchemy import Column, String, create_engine, Integer, Date
 from flask_sqlalchemy import SQLAlchemy
 import json
 
@@ -21,22 +21,48 @@ def setup_db(app, database_path=database_path):
 
 
 '''
-Person
-Have title and release year
+Movies
+have title and release date
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+class Movies(db.Model):
+    __tablename__ = "Movies"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    releaseDate = Column(Date)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+    def __init__(self, title, date):
+        self.title = title
+        self.date = date
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'date': self.date
+        }
+
+'''
+Actors
+have name, age, gender
+'''
+
+class Actors(db.Model):
+    __tablename__ = "Actors"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
